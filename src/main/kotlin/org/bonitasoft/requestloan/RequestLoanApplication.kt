dbd8499d0dd4c1eea3c251005051b607a3a65cdc
Baptiste Mesta <baptiste.mesta@gmail.com>
2019-04-12 16:55:01 +0200

    WIP: add first version of the process

---
+++ src/main/kotlin/org/bonitasoft/requestloan/RequestLoanApplication.kt
@@ -0,0 +1,11 @@
+package org.bonitasoft.requestloan
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RequestLoanApplication
+
+fun main(args: Array<String>) {
+	runApplication<RequestLoanApplication>(*args)
+}
