0d976240478540ab78c914cbeeccb1b962f59548
Baptiste Mesta <baptiste.mesta@gmail.com>
2019-04-17 14:38:36 +0200

    cleanup

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,22 +9,17 @@ plugins {
 repositories {
     mavenLocal()
     jcenter()
-    maven("http://repositories.rd.lan/maven/all/")
 }
 
 dependencies {
     implementation("org.bonitasoft.engine.dsl:process-kotlin-dsl:0.0.1")
     implementation("org.bonitasoft.engine:bonita-engine-spring-boot-starter:0.0.1")
     implementation("org.bonitasoft.connectors:bonita-connector-email-dsl:1.1.0")
+
     implementation("org.springframework.boot:spring-boot-starter-web:2.1.4.RELEASE")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.9.8")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("io.github.microutils:kotlin-logging:1.6.24")
-
-//    testImplementation("org.springframework.boot:spring-boot-starter-test:2.1.4.RELEASE")
-    testImplementation("junit:junit:4.12")
-    testImplementation("org.assertj:assertj-core:3.12.2")
 }
 
 
