5074ff91b0eae81e00062366ebb1adee5937b84b
Baptiste Mesta <baptiste.mesta@gmail.com>
2019-05-14 16:16:35 +0200

    first version with java api

--- src/main/kotlin/org/bonitasoft/requestloan/RequestLoan.kt
+++ src/main/kotlin/org/bonitasoft/requestloan/RequestLoan.kt
@@ -1,94 +1,71 @@
 package org.bonitasoft.requestloan
 
-import bonita.connector.email.email
-import org.bonitasoft.engine.dsl.process.*
-import org.bonitasoft.engine.dsl.process.ExpressionDSLBuilder.ExpressionDSLBuilderObject.constant
-import org.bonitasoft.engine.dsl.process.ExpressionDSLBuilder.ExpressionDSLBuilderObject.contractValue
-import org.bonitasoft.engine.dsl.process.ExpressionDSLBuilder.ExpressionDSLBuilderObject.dataRef
-import org.bonitasoft.engine.dsl.process.ExpressionDSLBuilder.ExpressionDSLBuilderObject.groovy
-import org.bonitasoft.engine.dsl.process.ExpressionDSLBuilder.ExpressionDSLBuilderObject.parameter
-import org.bonitasoft.engine.dsl.process.ExpressionDSLBuilder.ExpressionDSLBuilderObject.stringSubstitution
-import org.bonitasoft.engine.expression.ExpressionConstants
-import org.bonitasoft.engine.spring.BonitaProcessBuilder
-import org.bonitasoft.engine.spring.annotations.BonitaProcess
+import org.bonitasoft.engine.bpm.bar.BusinessArchive
+import org.bonitasoft.engine.bpm.bar.BusinessArchiveBuilder
+import org.bonitasoft.engine.bpm.bar.actorMapping.Actor
+import org.bonitasoft.engine.bpm.bar.actorMapping.ActorMapping
+import org.bonitasoft.engine.bpm.contract.Type
+import org.bonitasoft.engine.bpm.flownode.GatewayType
+import org.bonitasoft.engine.bpm.process.impl.ProcessDefinitionBuilder
+import org.bonitasoft.engine.expression.ExpressionBuilder
+import org.bonitasoft.engine.operation.OperationBuilder
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class RequestLoan {
+
+    @Bean
+    fun requestLoanProcess(): BusinessArchive {
+
+        val builder = ProcessDefinitionBuilder().createNewInstance("Request Loan", "1.0")
+
+        builder.setActorInitiator("requester")
+        builder.addActor("validator")
+        builder.addData("type", "java.lang.Integer", null)
+        builder.addData("amount", "java.lang.Integer", null)
+        builder.addData("accepted", "java.lang.Boolean", null)
+        builder.addData("reason", "java.lang.String", null)
+
+        val contractBuilder = builder.addContract()
+        contractBuilder.addInput("type", Type.TEXT, "type of the loan")
+        contractBuilder.addInput("amount", Type.INTEGER, "amount of the loan")
+
+        val reviewRequest = builder.addUserTask("Review request", "validator")
+        val reviewRequestContract = reviewRequest.addContract()
+        reviewRequestContract.addInput("accept", Type.BOOLEAN, "whether the loan is accepted or not")
+        reviewRequestContract.addInput("reason", Type.TEXT, "why the loan was accepted/rejected")
+        reviewRequest.addOperation(OperationBuilder().createSetDataOperation("accepted",
+                ExpressionBuilder().createContractInputExpression("accept", "java.lang.Boolean")))
+        reviewRequest.addOperation(OperationBuilder().createSetDataOperation("reason",
+                ExpressionBuilder().createContractInputExpression("reason", "java.lang.String")))
+
+        builder.addGateway("isAccepted", GatewayType.EXCLUSIVE)
+
+        builder.addUserTask("Sign contract", "requester")
+
+        builder.addAutomaticTask("Notify reject")
 
 
-@BonitaProcess
-class RequestLoan : BonitaProcessBuilder {
-    override fun build(): Process = process("Request Loan", "1.0") {
-        val requester = initiator("requester")
-        val validator = actor("validator")
-        data {
-            text named "type"
-            integer named "amount"
-            boolean named "accepted"
-            text named "reason"
+        builder.addTransition("Review request", "isAccepted")
+        builder.addTransition("isAccepted", "Sign contract", ExpressionBuilder().createDataExpression("accepted", "java.lang.Boolean"))
+        builder.addDefaultTransition("isAccepted", "Notify reject")
+
+
+        val designProcessDefinition = builder.done()
+
+        val businessArchiveBuilder = BusinessArchiveBuilder().createNewBusinessArchive()
+        businessArchiveBuilder.setProcessDefinition(designProcessDefinition)
+        businessArchiveBuilder.actorMapping = ActorMapping().apply {
+            addActor(Actor("requester").apply {
+                addUser("john")
+            })
+            addActor(Actor("validator").apply {
+                addUser("jack")
+            })
         }
-        contract {
-            text named "type" withDescription "type of the loan"
-            integer named "amount" withDescription "amount of the loan"
-        }
-        val review = userTask("Review request") {
-            actor = validator
-            contract {
-                boolean named "accept" withDescription "whether the load is accepted or not"
-                text named "reason" withDescription "why the loan was accepted/rejected"
-            }
-            operations {
-                update("accepted").with(contractValue("accept"))
-                update("reason").with(contractValue("reason"))
-            }
-        }
-        val gate = exclusiveGateway("isAccepted")
-        val sign = userTask("Sign contract") {
-            actor = requester
-        }
-        val notify = automaticTask("Notify reject") {
-            connector {
-                email {
-                    smtpHost(parameter("smtpHost"))
-                    smtpPort(constant(2525))
-                    from(constant("no-reply@acme.com"))
-                    to(groovy("""
-                                |def userId = apiAccessor.getProcessAPI().getProcessInstance(processInstanceId).getStartedBy()
-                                |return apiAccessor.getIdentityAPI().getUserWithProfessionalDetails(userId).contactData.email
-                            """.trimMargin()) {
-                        dependency(ExpressionDSLBuilder().apply { engineConstant(ExpressionConstants.PROCESS_INSTANCE_ID) })
-                        dependency(ExpressionDSLBuilder().apply { engineConstant(ExpressionConstants.API_ACCESSOR) })
-                    })
-                    subject(constant("Your loan was rejected"))
-                    message(stringSubstitution("""
-                        |
-                        | We are sorry to inform you that your Loan was rejected because:
-                        | ${'$'}{reason}
-                        |
-                        | Thank you
-                        |
-                    """.trimMargin()) {
-                        dataRef("reason")
-                    })
-                    sslSupport(constant(false))
-                }
-            }
-        }
-        transitions {
-            review to gate
-            gate to sign withCondition dataRef("accepted")
-            (gate to notify).isDefault()
-        }
+
+        return businessArchiveBuilder.done()
     }
 
-    override fun configuration(): ProcessConfiguration = configuration {
-        actorMapping {
-            "requester" to {
-                group("requesters")
-            }
-            "validator" to {
-                group("validators")
-            }
-        }
-        parameters {
-            "smtpHost" to "localhost"
-        }
-    }
 }
